6248deb82e25757fa9b68451d38c4fe5d2732dac
YesOhh <yxu_py@163.com>
2020-08-31 16:28:40 +0800

    fix bug

--- model/userModel.go
+++ model/userModel.go
@@ -8,40 +8,40 @@ import (
 )
 
 type LoginModel struct {
-	Email string `form:"email" binding:"email"`
+	Email    string `form:"email" binding:"email"`
 	Password string `form:"password"`
 }
 
 type RegisterModel struct {
-	Email string `form:"email" binding:"email"`
-	Password string `form:"password"`
+	Email         string `form:"email" binding:"email"`
+	Password      string `form:"password"`
 	PasswordAgain string `form:"passwordAgain"`
-	Nickname string `form:"nickname"`
+	Nickname      string `form:"nickname"`
 }
 
 type UserModel struct {
-	Id int64 `form:"id"`
-	Email string `form:"email" binding:"email"`
+	Id       int64  `form:"id"`
+	Email    string `form:"email" binding:"email"`
 	Password string `form:"password"`
 	Nickname string `form:"nickname"`
 }
 
 func (user *UserModel) SaveUser() int64 {
 	passwordHash := util.GenStringHash(user.Password)
-	result, e := initialization.Db.Exec("INSERT INTO " + initialization.DbName + "(email, password, nickname) values (?, ?, ?);", user.Email, passwordHash, user.Nickname)
+	result, e := initialization.Db.Exec("INSERT INTO "+initialization.DbName+"(email, password, nickname) values (?, ?, ?);", user.Email, passwordHash, user.Nickname)
 	if e != nil {
 		mylog.MyLogger.Panicln("user insert error", e.Error())
 	}
 	id, err := result.LastInsertId()
 	if err != nil {
-		mylog.MyLogger.Panicln("user insert id error", e.Error())
+		mylog.MyLogger.Panicln("user insert id error", err.Error())
 	}
 	return id
 }
 
 func (user *UserModel) QueryUserByEmail() UserModel {
 	u := UserModel{}
-	row := initialization.Db.QueryRow("SELECT id, email, password, nickname FROM " + initialization.DbName + " WHERE email = ?;", user.Email)
+	row := initialization.Db.QueryRow("SELECT id, email, password, nickname FROM "+initialization.DbName+" WHERE email = ?;", user.Email)
 	err := row.Scan(&u.Id, &u.Email, &u.Password, &u.Nickname)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -54,7 +54,7 @@ func (user *UserModel) QueryUserByEmail() UserModel {
 }
 
 func (user *UserModel) ValidUser() bool {
-	row := initialization.Db.QueryRow("SELECT password FROM " + initialization.DbName + " WHERE email = ?", user.Email)
+	row := initialization.Db.QueryRow("SELECT password FROM "+initialization.DbName+" WHERE email = ?", user.Email)
 	var passwordHash string
 	err := row.Scan(&passwordHash)
 	if err != nil {
